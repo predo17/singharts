e8f1cc3fb5c0b06f6ac2a78743a0dbd112f81e99
predo17 <150456318+predo17@users.noreply.github.com>
2023-12-07 20:07:31 -0300

    hola
    opa

--- meuSite/scripts.js
+++ meuSite/scripts.js
@@ -3,11 +3,14 @@ const cards =document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener ("click" ,function(){
+        const videosid = card.getAttribute ("id")
         modal_overlay.classList.add ("active")
+        modal_overlay.querySelector("iframe") .src =`https://www.youtube.com/embed/${videosid}`;
     })
 
 }
 document.querySelector (".close_modal").addEventListener("click" ,function(){
     modal_overlay.classList.remove("active")
+    modal_overlay.querySelector("iframe") .src ="";
 })
 
