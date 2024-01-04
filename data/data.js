c344186aa682c7db9c18467425be2b6ff372037e
predo17 <150456318+predo17@users.noreply.github.com>
2024-01-04 20:26:07 -0300

    data
    hello

---
+++ data/data.js
@@ -0,0 +1,19 @@
+module.exports=[{
+    id:"ca4ZW2rcmUM",
+    title:"LOKI VAI TRAZER TONY STARK E VIÚVA NEGRA DE...",
+    duration:"9:27 main",
+    price:"FREE",
+ },
+ {
+    id:"ca4ZW2rcmUM",
+    title:"LOKI VAI TRAZER TONY STARK E VIÚVA NEGRA DE...",
+    duration:"9:27 main",
+    price:"FREE",
+ },
+ {
+    id:"ca4ZW2rcmUM",
+    title:"LOKI VAI TRAZER TONY STARK E VIÚVA NEGRA DE...",
+    duration:"9:27 main",
+    price:"FREE",
+ },
+]
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require ('express')
 const server = express ()
 const nunjucks = require ('nunjucks')
+const videos = require("./data/data")
 
 server.use (express.static('public'))
-server.set ("view engine","html")
+server.set ("view engine","njk")
 
 nunjucks.configure("views",{
     express:server
@@ -12,11 +13,19 @@ nunjucks.configure("views",{
 server.listen(5000,function(){
     console.log("hello world, serve is runing save automatic")
 
-    server.get("/",function(req,res){
-       return res.render("index")
+    server.get("/in",function(req,res){
+        const about={
+            avatar_url:"https://avatars.githubusercontent.com/u/150456318?v=4",
+            nome:"Pedro Henrique",
+            role:" massancode",
+            description: "Desenvolvedor",
+            link:"http://www.github.com/dashboard",
+        }
+
+       return res.render("about",{about})
        
     })
     server.get("/videos",function(req,res){
-        return res.render("videos")
+        return res.render("videos",{items:videos})
     })
 })
